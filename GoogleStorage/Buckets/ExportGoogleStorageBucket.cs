c4f3b520a996c1e0a1ee7b256cb02f3d3bed180c
Don Kackman <dkackman@gmail.com>
2014-12-06 10:37:12 -0600

    refactor pipeline to be generic

--- GoogleStorage/Buckets/ExportGoogleStorageBucket.cs
+++ GoogleStorage/Buckets/ExportGoogleStorageBucket.cs
@@ -8,6 +8,8 @@ using System.Collections.Generic;
 
 using GoogleStorage.ProducerConsumer;
 
+using Newtonsoft.Json;
+
 namespace GoogleStorage.Buckets
 {
     [Cmdlet(VerbsData.Export, "GoogleStorageBucket", SupportsShouldProcess = true)]
@@ -37,15 +39,18 @@ namespace GoogleStorage.Buckets
                 var access_token = accessTask.Result;
                 IEnumerable<dynamic> items = contents.items;
 
-                using (var pipeline = new Stage<Tuple<dynamic, string>, Tuple<dynamic, string>>()
+                using (var downloadPipeline = new Stage<Tuple<dynamic, string>, Tuple<dynamic, string>>())
                 {
-                    UserAgent = GoogleStorageCmdlet.UserAgent,
-                    IncludeMetaData = IncludeMetaData
-                })
-                {
+                    // this is the delgate that does the downloading
+                    Func<Tuple<dynamic, string>, Tuple<dynamic, string>> func = (input) =>
+                        {
+                            Task<Tuple<dynamic, string>> task = ExportObject(input, cancelToken, access_token);
+                            return task.Result;
+                        };
+
                     // this kicks off a number of async tasks that will do the downloads
                     // as items are added to the Input queue
-                    pipeline.Start(cancelToken, access_token);
+                    downloadPipeline.Start(func, cancelToken);
 
                     bool yesToAll = false;
                     bool noToAll = false;
@@ -60,16 +65,19 @@ namespace GoogleStorage.Buckets
                                 var msg = string.Format("Do you want to overwrite the file {0}?", path);
                                 if (Force || ShouldContinue(msg, "Overwrite file?", ref yesToAll, ref noToAll))
                                 {
-                                    pipeline.Input.Add(tuple, cancelToken);
+                                    downloadPipeline.Input.Add(tuple, cancelToken);
                                 }
                             }
                         }
                         else
                         {
-                            pipeline.Input.Add(tuple, cancelToken);
+                            downloadPipeline.Input.Add(tuple, cancelToken);
                         }
                     }
-                    pipeline.Input.CompleteAdding();
+
+                    // all of the items are enumerated and queued for download
+                    // let the pipeline stage know that it can exit that enumeration when empty
+                    downloadPipeline.Input.CompleteAdding();
 
                     int count = items.Count();
                     int i = 0;
@@ -77,12 +85,12 @@ namespace GoogleStorage.Buckets
                     // those tasks populate this blocking collection
                     // it will block until all of the tasks are complete 
                     // at which point we know the background threads are done and the enumeration will complete
-                    foreach (var item in pipeline.Output.GetConsumingEnumerable(cancelToken))
+                    foreach (var item in downloadPipeline.Output.GetConsumingEnumerable(cancelToken))
                     {
                         WriteVerbose(string.Format("({0} of {1}) - Exported {2} to {3}", ++i, count, item.Item1.name, item.Item2));
                     }
 
-                    foreach (var tuple in pipeline.Errors)
+                    foreach (var tuple in downloadPipeline.Errors)
                     {
                         WriteError(new ErrorRecord(tuple.Item2, tuple.Item2.Message, ErrorCategory.ReadError, tuple.Item1.Item1));
                     }
@@ -101,6 +109,29 @@ namespace GoogleStorage.Buckets
             catch (Exception e)
             {
                 WriteError(new ErrorRecord(e, e.Message, ErrorCategory.ReadError, null));
+            }
+        }
+
+        private async Task<Tuple<dynamic, string>> ExportObject(Tuple<dynamic, string> item, CancellationToken cancelToken, string access_token)
+        {
+            var downloader = new FileDownloader(item.Item1.mediaLink, item.Item2, item.Item1.contentType, GoogleStorageCmdlet.UserAgent);
+
+            await downloader.Download(cancelToken, access_token);
+
+            if (IncludeMetaData)
+            {
+                SaveMetaData(item);
+            }
+
+            return item;
+        }
+
+        private static void SaveMetaData(Tuple<dynamic, string> item)
+        {
+            using (var writer = new StreamWriter(item.Item2 + ".metadata.json"))
+            {
+                string json = JsonConvert.SerializeObject(item.Item1);
+                writer.Write(json);
             }
         }
 
--- GoogleStorage/ProducerConsumer/Stage.cs
+++ GoogleStorage/ProducerConsumer/Stage.cs
@@ -2,35 +2,28 @@
 using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
-using System.IO;
-
-using Newtonsoft.Json;
 
 namespace GoogleStorage.ProducerConsumer
 {
     class Stage<TInput, TOutput> : IDisposable
     {
-        public BlockingCollection<Tuple<dynamic, string>> Input { get; private set; }
+        public BlockingCollection<TInput> Input { get; private set; }
 
-        public BlockingCollection<Tuple<dynamic, string>> Output { get; private set; }
+        public BlockingCollection<TOutput> Output { get; private set; }
 
-        public ConcurrentBag<Tuple<Tuple<dynamic, string>, Exception>> Errors { get; private set; }
+        public ConcurrentBag<Tuple<TInput, Exception>> Errors { get; private set; }
 
-        public int ThreadCount { get; set; }
-
-        public string UserAgent { get; set; }
-
-        public bool IncludeMetaData { get; set; }
+        public int TaskCount { get; set; }
 
         public Stage()
         {
-            ThreadCount = 5;
-            Input = new BlockingCollection<Tuple<dynamic, string>>();
-            Output = new BlockingCollection<Tuple<dynamic, string>>();
-            Errors = new ConcurrentBag<Tuple<Tuple<dynamic, string>, Exception>>();
+            TaskCount = 4;
+            Input = new BlockingCollection<TInput>();
+            Output = new BlockingCollection<TOutput>();
+            Errors = new ConcurrentBag<Tuple<TInput, Exception>>();
         }
 
-        public void Start(CancellationToken cancelToken, string access_token)
+        public void Start(Func<TInput, TOutput> func, CancellationToken cancelToken)
         {
             // this is the delegate that does the downloading
             Action download = () =>
@@ -39,8 +32,7 @@ namespace GoogleStorage.ProducerConsumer
                     {
                         try
                         {
-                            var exportTask = ExportObject(item, cancelToken, access_token);
-                            Output.Add(exportTask.Result);
+                            Output.Add(func(item));
                         }
                         catch (AggregateException e)
                         {
@@ -59,38 +51,15 @@ namespace GoogleStorage.ProducerConsumer
             // these are the download threads - each download blocks while in progress but the others can work in parallel
             Task.Run(() =>
                 {
-                    Task[] tasks = new Task[ThreadCount];
+                    Task[] tasks = new Task[TaskCount];
                     for (int i = 0; i < tasks.Length; i++)
                     {
                         tasks[i] = Task.Run(download, cancelToken);
                     }
 
                     Task.WaitAll(tasks, cancelToken);
-                    Output.CompleteAdding(); // this signals the calling thread that work is done
+                    Output.CompleteAdding(); // this signals the calling thread that all the work is done
                 }, cancelToken);
-        }
-
-        private async Task<Tuple<dynamic, string>> ExportObject(Tuple<dynamic, string> item, CancellationToken cancelToken, string access_token)
-        {
-            var downloader = new FileDownloader(item.Item1.mediaLink, item.Item2, item.Item1.contentType, UserAgent);
-
-            await downloader.Download(cancelToken, access_token);
-
-            if (IncludeMetaData)
-            {
-                SaveMetaData(item);
-            }
-
-            return item;
-        }
-
-        private void SaveMetaData(Tuple<dynamic, string> item)
-        {
-            using (var writer = new StreamWriter(item.Item2 + ".metadata.json"))
-            {
-                string json = JsonConvert.SerializeObject(item.Item1);
-                writer.Write(json);
-            }
         }
 
         public void Dispose()
